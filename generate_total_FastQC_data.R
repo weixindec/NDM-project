d64b08a6b36fea1df84e67bc7c3a3583edb54a6c
weixindec <weixin_dec@yahoo.com>
2016-05-24 10:37:41 +0800

    Update generate_total_FastQC_data.R

--- generate_total_FastQC_data.R
+++ generate_total_FastQC_data.R
@@ -1,4 +1,6 @@
 files = list.files(pattern="*.txt")
+#alternatively- temp = list.files(pattern="*.csv")
+#for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i]))
 myfiles = do.call(rbind, lapply(files, function(x) read.delim(x, stringsAsFactors = FALSE,skip=12,nrows=27,header=TRUE)))
 colname = c("Base", "Mean",	"Median", "Lower_Quartile",	"Upper_Quartile",	"10th_Percentile", "90th_Percentile")
 colnames(myfiles) = colname
@@ -12,3 +14,5 @@ total_FastQC = summarise(grouped,
       new_10th_percentile = quantile(Median, probs = 0.10),
       new_90th_percentile = quantile(Median, probs = 0.90)
     )
+    
+    
